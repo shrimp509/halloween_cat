2ce2d187094b4b1d04ac145ad8a7fd13017f8e62
UncleSam <shrimp509@hotmail.com>
2022-10-30 13:00:39 +0800

    Fix redis host

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:5432' }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST'] || 'localhost'}:6379/1" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST'] || 'localhost'}:6379/1" }
 end
